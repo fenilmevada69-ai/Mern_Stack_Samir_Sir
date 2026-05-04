1528840e97f450fab795425833a4294a937a7976
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-04 17:43:17 +0530

    date

--- src/components/InputDemo2.jsx
+++ src/components/InputDemo2.jsx
@@ -3,6 +3,46 @@ import React, { useState } from "react";
 export const InputDemo2 = () => {
   const [country, setcountry] = useState("");
   const [gender, setgender] = useState("");
+  const [selectedDate, setselectedDate] = useState("")
+  const [result, setresult] = useState("")
+  const dateHandler = (e)=>{
+    console.log(e.target.value)
+    const value = e.target.value
+    setselectedDate(value)
+
+    const inputDate = new Date(value) //date object
+    const today = new Date() //today
+
+    //remove time
+    inputDate.setHours(0,0,0,0)
+    today.setHours(0,0,0,0)
+
+    const difftime = inputDate-today; //mills
+    console.log(difftime)
+    const diffDays = difftime/ (1000*60*60*24)
+    console.log(diffDays)
+
+    if(diffDays==0){
+      setresult("today")
+    }
+    else if(diffDays== -1){
+      setresult("yesterday")
+    }
+    else if(diffDays== 1){
+      setresult("tomorrow")
+    }
+    else if(diffDays<0){
+      setresult(`${Math.abs(diffDays)} days ago`)
+    }
+    else{
+      setresult(`${Math.abs(diffDays)} days later`)
+    }
+
+
+
+
+
+  }
   return (
     <div style={{ textAlign: "center" }}>
       <h1>INPUT DEMO 2</h1>
@@ -40,6 +80,11 @@ export const InputDemo2 = () => {
           }}
         ></input>
         {gender}
+      </div>
+      <div>
+        <label>SELECT DATE</label>
+        <input type="date" onChange={(e)=>{dateHandler(e)}}></input>
+        <h1>{result}</h1>
       </div>
     </div>
   );
