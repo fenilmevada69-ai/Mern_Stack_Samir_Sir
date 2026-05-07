f0ea5de25df108b2783755fd97c56d47a9d3960a
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-07 10:13:02 +0530

    some commit

--- src/components/InputDemo1.jsx
+++ src/components/InputDemo1.jsx
@@ -1,58 +1,50 @@
-import React, { useState } from 'react'
-
+import {useState} from "react";
 export const InputDemo1 = () => {
-    //input onChange event
-    const [name, setname] = useState("")
-    const [email, setemail] = useState("")
-    const [address, setaddress] = useState("")
-    const [isClicked, setisClicked] = useState(false)
 
-    const nameHandler = (event)=>{
-        //console.log("name handler called...",event)
-        console.log(event.target.value)
-        setname(event.target.value) //updating name value by using setname
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [isClicked, setIsClicked] = useState(false);
+    const nameHandler = (e) => {
+        console.log(e.target.value);
+        setName(e.target.value);
     }
-    const emailHandler = (e)=>{
-        //e == event
-        console.log(e.target.value)
-        setemail(e.target.value)
+    const emailHandler = (e) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
     }
-    
-  return (
-    <div style={{textAlign:"center"}}>
-        <h1>INPUT DEMO 1</h1>
-        <div>
-            <label>NAME :</label>
-            <input type='text' onChange={(event)=>{nameHandler(event)}}></input>
-            {/* {name} */}
-        </div>
-        <div>
-            <label>EMAIL :</label>
-            <input type='text' onChange={(e)=>{emailHandler(e)}}></input>
-            {/* {email} */}
-        </div>
-        
-        <div>
-            <label>Address :</label>
-            <input type='text' onChange={(event)=>{setaddress(event.target.value)}}></input>
-            {/* {address} */}
-        </div>
-        <div>
-            <label>COLOR</label>
-            <input type='color'></input>
-        </div>
-        <button onClick={()=>{setisClicked(true)}}>CLICK</button>
+    const clearDetails = () => {
+        setName("");
+        setEmail("");
+        setAddress("");
+        setIsClicked(false);
+    }
 
-        {
-        isClicked == true && 
-        <div>
-            <h1>OUTPUT</h1>
-            <h1>Name = {name}</h1>
-            <h2>Email = {email}</h2>
-            <h3>Address ={address}</h3>
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>INPUT DEMO 1</h1>
+            <div>
+                <label htmlFor="name">Name: </label>
+                <input value={name} type="text" id="name" onChange={(e) => {nameHandler(e)}}/>
+            </div>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input type="email" onChange={(e) => {emailHandler(e)}} id="email"/>
+            </div>
+            <div>
+                <label htmlFor="address">Address: </label>
+                <input type="text" onChange={(e) => {setAddress(e.target.value)}} id="address"/>
+            </div>
+            <button onClick={() => {setIsClicked(true)}}>Click Me</button>
+            <button onClick={clearDetails}>Unclick</button>
+            {
+                isClicked == true &&
+                <div>   
+                    <h1>Name: {name}</h1>
+                    <h1>Email: {email}</h1>
+                    <h1>Address: {address}</h1>
+                </div>
+            }
         </div>
-    }
-        
-    </div>
-  )
+    )
 }
--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -6,6 +6,6 @@ export const MyButton = (props) => {
     //<button style={{backgroundColor:"green"}}>{props.name}</button>
     //<button style={{backgroundColor:"green"}}>{props.name || "Test"}</button>
     //<button style={{backgroundColor:props.bg || "blue"}}>{props.name || "Test"}</button>
-    <button onClick={()=>{props.func()}} style={{backgroundColor:props.bg || "blue"}}>{props.name || "Test"}</button>
+    <button onClick={()=>{props.func()}} style={{backgroundColor:props.bg || "blue"}}>{"Test"}</button>
   )
 }
