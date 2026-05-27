797a1bfd6373cdfbf3e9ad474051de97b66c409c
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-05-27 10:46:23 +0530

    some message

--- src/components/Form/TaskDemo.jsx
+++ src/components/Form/TaskDemo.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 
 export const TaskDemo = () => {
+    const [userData, setuserData] = useState({})
+    const [isClicked, setisClicked] = useState(false)
+
     const {register,
         handleSubmit,
     } = useForm();
 
     const submitHandler = (data) => {
         console.log("data..",data)
+        setuserData(data)
     }
     return (
         <div style={{textAlign:"center"}}>
@@ -28,15 +32,24 @@ export const TaskDemo = () => {
                         <input type="radio" id="female" value='female' {...register("gender")}/>
                     </div>
                     <div>
-                        <select name="country" id="country">
+                        <select name="country" id="country" {...register("country")}>
                             <option disabled value="">Selected</option>
                             <option value="india">India</option>
                             <option value="china">China</option>
                             <option value="russia">Russia</option>
                         </select>
                     </div>
-                    <input type="submit" value="Submit Me"/>
+                    <input type="submit" value="Submit Me" onClick={() => setisClicked(true)}/>
                 </form>
+                {
+                    isClicked && <div className="props">
+                        <h1>Output</h1>
+                        <h1>Name = {userData.name}</h1>
+                        <h1>Age = {userData.age}</h1>
+                        <h1>Gender = {userData.gender}</h1>
+                        <h1>Country = {userData.country}</h1>
+                    </div>
+                }
             </div>
     )
 }
